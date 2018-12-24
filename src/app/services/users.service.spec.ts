62ae69e5010c4c81505231189ad2a0ba6a2e8b5a
Luis Juarros <luis.juarros@inddubio.com>
2018-12-24 05:01:59 +0100

    registration added

--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,12 +1,44 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule, HttpRequest, HttpParams } from '@angular/common/http';
 
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => 
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        UsersService
+      ]  
+    })
+  );
+
+  afterEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+    backend.verify();
+  }));
 
   it('should be created', () => {
     const service: UsersService = TestBed.get(UsersService);
     expect(service).toBeTruthy();
   });
+
+  it(`should send an expected register request`, async(inject([UsersService, HttpTestingController],
+    (service: UsersService, backend: HttpTestingController) => {
+      const valid_user:any = {"username":"username", "password":"password"};
+      service.createUser(valid_user).subscribe();
+
+      backend.expectOne((req: HttpRequest<any>) => {
+        const body = new HttpParams({ fromObject: req.body });
+
+        return req.url === 'https://apitrello.herokuapp.com/users'
+          && req.method === 'POST'
+          && req.headers.get('Content-Type') === 'application/json'
+          && body.get('username') === 'username'
+          && body.get('password') === 'password';
+      }, `POST to 'users' with user and password`);
+  })));
 });
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private usersApi = "https://apitrello.herokuapp.com/users";
+  
+  constructor(private httpClient: HttpClient) {}
+
+  public createUser(data:any): Observable<any> {
+    return this.httpClient.post<any>(this.usersApi, data, httpOptions)
+  }
 }
