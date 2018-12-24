b6898ebd0dc30998d4514fd15206304e0026954d
Luis Juarros <luis.juarros@inddubio.com>
2018-12-24 05:05:17 +0100

    user-registration component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './users/user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
