f418e3f8b2a87795a4a7ade74c99871004fca4d9
Luis Juarros <luis.juarros@inddubio.com>
2018-12-25 14:18:55 +0100

    list creation ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppComponent } from './app.component';
 import { UserRegistrationComponent } from './users/user-registration/user-registration.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UsersService } from './services/users.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UserLoginComponent } from './users/user-login/user-login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
 import { UsersGuardService } from './services/users-guard.service';
 import { ListsComponent } from './lists/lists.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { InterceptorService } from './services/interceptor.service';
 
 export function tokenGetter() {
   return localStorage.getItem('id_token');
@@ -38,7 +39,11 @@ export function tokenGetter() {
     }),
     DragDropModule
   ],
-  providers: [UsersService, UsersGuardService, JwtHelperService],
+  providers: [UsersService, UsersGuardService, JwtHelperService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: InterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
