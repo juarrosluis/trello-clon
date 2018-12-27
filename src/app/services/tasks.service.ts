f273aedcc20818499ab46b163277c58ea99d81d7
Luis Juarros <luis.juarros@inddubio.com>
2018-12-27 13:55:16 +0100

    all CRUD methods created

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  private tasksApi = "http://localhost:1337/apitrello.herokuapp.com/taks";
+  private listTasksApi = "http://localhost:1337/apitrello.herokuapp.com/lists/taks";
+  
+  constructor(private httpClient: HttpClient) { }
+
+  createTaskOnAList(data): Observable<any> {
+    return this.httpClient.post<any>(this.tasksApi, data, httpOptions)
+  }
+
+  retrieveAllTasksOfAList(listID:number): Observable<any> {
+    return this.httpClient.get<any>(this.listTasksApi + "/" + listID, httpOptions)
+  }
+
+  deleteAllTasksOfAList(listID:number){
+    return this.httpClient.delete(this.tasksApi + "/" + listID, httpOptions);
+  }
+
+  retrieveTask(id:number): Observable<any> {
+    return this.httpClient.get<any>(this.tasksApi + "/" + id, httpOptions)
+  }
+
+  updateTask(data, id): Observable<any> {
+    return this.httpClient.put<any>(this.tasksApi + "/" + id, data, httpOptions)
+  }
+
 }
